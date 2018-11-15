bae512a5436899f03b66b55c82e22df4cfb6dd51
joecoker <joe_coker@hotmail.co.uk>
2018-11-15 14:40:06 +0000

    PLayer clas added

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,4 +4,11 @@ feature "Attack reduces hitpoints" do
     click_button 'Attack'
     expect(page).to have_text("Joe: 90HP")
   end
+
+  scenario "Clicking Play button returns to main page" do
+    sign_in_and_play()
+    click_button 'Attack'
+    click_button 'Play'
+    expect(page).to have_text("Will vs Joe")
+  end
 end
